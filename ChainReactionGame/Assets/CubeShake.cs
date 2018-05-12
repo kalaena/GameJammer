6f191ebfcf484b39aefa1ecd9079e8138103c430
joester <joey_shepard@sbcglobal.net>
2018-05-12 00:41:36 -0700

    Created BuildingTest Prefab
    Also added forceScalar (CubeShake script) to each cube of the Test Building.

--- ChainReactionGame/Assets/CubeShake.cs
+++ ChainReactionGame/Assets/CubeShake.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class CubeShake : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        int scalar = 5;
-		gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(1,3), Random.Range(1,3), Random.Range(1,3)) * scalar);
+    [Tooltip("How much force is applied to the GameObject on Start")]
+    public int forceScalar = 3; 
+    
+    void Start () {      
+        //at runtime, randomly generate force and increase it by a factor of forceScalar
+		gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(1,3), Random.Range(1,3), Random.Range(1,3)) * forceScalar);
     }
 	
 	// Update is called once per frame
