459ab401a9b1b2a678c6d04a438a3bf0d4ebb658
joester <joey_shepard@sbcglobal.net>
2018-05-13 13:51:44 -0700

    meta files for building + added my scene + detonate button full juice

--- ChainReactionGame/Assets/Scripts/BombManagerScript.cs
+++ ChainReactionGame/Assets/Scripts/BombManagerScript.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BombManagerScript : MonoBehaviour {
 
     public int maxNumberOfBombsAllowed;
     public int remainingBombs;
+    public GameObject detonateButton;
 
     public List<GameObject> bombsPlaced;
 
@@ -13,6 +15,7 @@ public class BombManagerScript : MonoBehaviour {
 	void Start () {
         remainingBombs = maxNumberOfBombsAllowed = 3;
         bombsPlaced = new List<GameObject>();
+        detonateButton.GetComponent<Button>().interactable = false;
 	}
 	
 	// Update is called once per frame
@@ -23,6 +26,7 @@ public class BombManagerScript : MonoBehaviour {
     public void bombPlaced() 
     {
         remainingBombs--;
+        detonateButton.GetComponent<Button>().interactable = true;
     }
 
     public void detonateBombs() {
@@ -31,5 +35,6 @@ public class BombManagerScript : MonoBehaviour {
             bomb.GetComponent<BombController>().explode();
         }
         bombsPlaced.Clear();
+        detonateButton.GetComponent<Button>().interactable = false;
     }
 }
