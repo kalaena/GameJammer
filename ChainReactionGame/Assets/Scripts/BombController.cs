8a66bdcd804770575c6710c7adcd130b694fe123
joester <joey_shepard@sbcglobal.net>
2018-05-12 18:11:03 -0700

    undo scene changes
    from merge

--- ChainReactionGame/Assets/Scripts/BombController.cs
+++ ChainReactionGame/Assets/Scripts/BombController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class BombController : MonoBehaviour
 {
     public GameObject explosionPrefab;
-    public GameObject cubeUnlocker;
 
     private GameObject explosionInstance;
 
@@ -31,11 +30,6 @@ public class BombController : MonoBehaviour
 
         //create explosion at our bomb's location
         explosionInstance = Instantiate(explosionPrefab, transform.position, Quaternion.Euler(0, 90, 0));
-        explosionInstance.transform.LookAt(GameObject.Find("Main Camera").transform.position);
-
-        //create a sphere collider that adds rigidbody collision to all cubes it touches
-        cubeUnlocker = Instantiate(cubeUnlocker, transform.position, Quaternion.Euler(0, 90, 0));
-        
 
         //we can destroy the game object just yet, because we have to wait for the explosion to cleanup... so move it out of view for a moment until we destroy it
         gameObject.transform.position = new Vector3(999, 999, 999);
