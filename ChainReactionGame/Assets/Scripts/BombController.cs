a6b28c85675453fcf9855ee25549db77eb357c6d
joester <joey_shepard@sbcglobal.net>
2018-05-12 18:08:50 -0700

    Added "brick by brick" physics bomb explosions
    Added a tag "Immobile" which is attached to game objects we want bomb explosions to ignore.

--- ChainReactionGame/Assets/Scripts/BombController.cs
+++ ChainReactionGame/Assets/Scripts/BombController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BombController : MonoBehaviour
 {
     public GameObject explosionPrefab;
+    public GameObject cubeUnlocker;
 
     private GameObject explosionInstance;
 
@@ -30,6 +31,11 @@ public class BombController : MonoBehaviour
 
         //create explosion at our bomb's location
         explosionInstance = Instantiate(explosionPrefab, transform.position, Quaternion.Euler(0, 90, 0));
+        explosionInstance.transform.LookAt(GameObject.Find("Main Camera").transform.position);
+
+        //create a sphere collider that adds rigidbody collision to all cubes it touches
+        cubeUnlocker = Instantiate(cubeUnlocker, transform.position, Quaternion.Euler(0, 90, 0));
+        
 
         //we can destroy the game object just yet, because we have to wait for the explosion to cleanup... so move it out of view for a moment until we destroy it
         gameObject.transform.position = new Vector3(999, 999, 999);
