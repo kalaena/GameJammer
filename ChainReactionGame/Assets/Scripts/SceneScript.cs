d0535609b1041e7c048ed0230248ee4df8d5a0bc
joester <joey_shepard@sbcglobal.net>
2018-05-13 15:25:51 -0700

    meta files and recycling level select to credits button

--- ChainReactionGame/Assets/Scripts/SceneScript.cs
+++ ChainReactionGame/Assets/Scripts/SceneScript.cs
@@ -8,24 +8,17 @@ public class SceneScript : MonoBehaviour {
     public GameObject SoundManager;
 
 	// Use this for initialization
-	void Start () {
-        DontDestroyOnLoad(this.gameObject);
-        
-	}
+    void Start()
+    {
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (SoundManager == null)
-            GameObject.Destroy(this.gameObject);
+        
 	}
 
     public void loadScene(int sceneID) 
     {
-        if (sceneID == 0 || sceneID == 2)
-        {
-            if (SoundManager != null)
-                DontDestroyOnLoad(SoundManager);
-        }
         SceneManager.LoadScene(sceneID);
     }
 }
