e8274620bf4843fc55275baae7b60c04b25a314a
joester <joey_shepard@sbcglobal.net>
2018-05-13 18:39:41 -0700

    more point notification code

---
+++ ChainReactionGame/Assets/Scripts/HighScoreUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreUI : MonoBehaviour {
+
+    public GameObject DataController;
+
+	// Use this for initialization
+	void Start () {
+        refreshHighScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void refreshHighScore()
+    {
+        this.gameObject.GetComponent<Text>().text = "High Score: " + DataController.GetComponent<DataController>().GetHighestPlayerScore();
+    }
+}
---
+++ ChainReactionGame/Assets/Scripts/PointUIScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointUIScript : MonoBehaviour {
+
+    public int pointValue;
+    public float originalYposition;
+    public const float fadeSpeed = 3;
+
+	// Use this for initialization
+	void Start () {
+		pointValue = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //slowly move up until death
+        this.gameObject.transform.Translate(new Vector3(0,1,0));
+        if (this.gameObject.transform.position.y - originalYposition > 5)
+            GameObject.Destroy(this.gameObject);
+
+        Text text = this.gameObject.GetComponent<Text>();
+        //fade away
+        if (text.color != Color.clear)
+            Color.Lerp(text.color, Color.clear, fadeSpeed * Time.deltaTime);
+
+	}
+
+    public void setPointValue(int pointValue) 
+    {
+        this.gameObject.GetComponent<Text>().text = "+ " + pointValue;
+    }
+}
