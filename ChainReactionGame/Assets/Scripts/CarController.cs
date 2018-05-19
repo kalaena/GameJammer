276da2be9589e67a84a342ad28a0a41437950d2c
joester <joey_shepard@sbcglobal.net>
2018-05-19 16:43:00 -0700

    scoremanager mass assign

--- ChainReactionGame/Assets/Scripts/CarController.cs
+++ ChainReactionGame/Assets/Scripts/CarController.cs
@@ -52,10 +52,13 @@ public class CarController : MonoBehaviour
         //pick random direction
         bool direction = Random.Range(0, 2) == 1; //0-->1 but using 2 because max is exclusive for some reason
 
-        GameObject car = Instantiate(CarPrefabs[prefabIndex], new Vector3(direction ? -0.65f : -0.275f, 0.15f, direction ? RightEdgeOfMap : LeftEdgeOfMap), direction ? Quaternion.Euler(0.0f, 180.0f, 0.0f) : Quaternion.identity);
-        Rigidbody rb = car.GetComponent<Rigidbody>();
-        rb.velocity = new Vector3(0.0f, 0.0f, direction ? -speed : speed);
-        spawnedCars.Add(car);
+        if (CarPrefabs[prefabIndex] != null)
+        {
+            GameObject car = Instantiate(CarPrefabs[prefabIndex], new Vector3(direction ? -0.65f : -0.275f, 0.15f, direction ? RightEdgeOfMap : LeftEdgeOfMap), direction ? Quaternion.Euler(0.0f, 180.0f, 0.0f) : Quaternion.identity);
+            Rigidbody rb = car.GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(0.0f, 0.0f, direction ? -speed : speed);
+            spawnedCars.Add(car);
+        }
     }
 
     public void OnCarDestroy(GameObject car)
