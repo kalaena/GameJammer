5c21518478cad8dfc8f96a2b82c4e54cf53fe0df
joester <joey_shepard@sbcglobal.net>
2018-05-19 15:49:05 -0700

    added camera shake, removed old unused camera shake script
    also put matthews file in the Colors folder.

--- ChainReactionGame/Assets/Scripts/BombManagerScript.cs
+++ ChainReactionGame/Assets/Scripts/BombManagerScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using EZCameraShake;
 
 public class BombManagerScript : MonoBehaviour {
 
@@ -23,6 +24,7 @@ public class BombManagerScript : MonoBehaviour {
 		
 	}
 
+    //Enable the detonate button to be pressed and decrement the remaining bombs in the UI
     public void bombPlaced() 
     {
         remainingBombs--;
@@ -30,12 +32,18 @@ public class BombManagerScript : MonoBehaviour {
     }
 
     public void detonateBombs() {
-        foreach (GameObject bomb in bombsPlaced)
-        {
+        //Shake the camera when bombs are detonated, scaled off the number of bombs that exploded
+        CameraShaker.Instance.ShakeOnce(5.0f * bombsPlaced.Count, 3.0f, 0.1f, 2.0f);
+        
+        //Explode each bomb placed in the level
+        foreach (GameObject bomb in bombsPlaced) {
             bomb.GetComponent<BombController>().explode();            
         }
+
+        //Remove the bombs from the list, deactivating the detonate button
         bombsPlaced.Clear();
         detonateButton.GetComponent<Button>().interactable = false;
+
 
     }
 }
